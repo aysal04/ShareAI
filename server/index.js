fbb8115ca623e5e4b80814fc78cb10c83504f1d3
lasya_kata <109300562+aysal04@users.noreply.github.com>
2024-10-09 00:11:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,20 @@ import posts from "./routes/Posts.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.CLIENT_BASE_URL}`,
+    methods: ["GET", "POST"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
@@ -43,10 +56,12 @@ const connectDB = () => {
     });
 };
 
+const PORT = process.env.PORT || 8080;
+
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
